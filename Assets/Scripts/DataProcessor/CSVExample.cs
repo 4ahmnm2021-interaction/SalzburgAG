cbc63dd7434908b61354c6cfe241d56ea6d76791
kilianreisinger <56028726+kilianreisinger@users.noreply.github.com>
2021-04-06 13:59:32 +0200

    csv Data Processor

---
+++ Assets/Scripts/DataProcessor/CSVExample.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CsvHelper;
+using System.IO;
+
+public class CSVExample : MonoBehaviour
+{
+    public Freischaltschein MySchein = new Freischaltschein();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var reader = new StreamReader("C:\\Users\\kilia\\Downloads\\CSV\\sample.csv");
+        var csv = new CsvReader(reader);
+
+        csv.Configuration.Delimiter = ";";
+
+        var records = csv.GetRecords<Anlage>();
+
+        var fetch = csv.GetRecords<FreischaltscheinFetch>();
+
+        // MySchein.Anlagen = records;
+      
+
+        foreach (var item in records)
+        {
+            // Debug.Log("ID: " + item.KKS + " / Name: " + item.Bezeichnung);
+       
+        }
+
+   
+       
+        
+    }
+
+}
+
+public class Anlage
+{
+    public string KKS{ get; set; }
+    public string Bezeichnung { get; set; }
+
+    public string Ort { get; set; }
+
+    public string IST { get; set; }
+
+    public string SOLL { get; set; }
+}
+
+
+public class Freischaltschein
+{
+    public int Nummer{ get; set; }
+    public string Datum{ get; set; }
+
+    public Anlage[] Anlagen;
+
+
+}
+
+public class FreischaltscheinFetch
+{
+    public string Nummer{ get; set; }
+    public string Datum{ get; set; }
+}
+
+
---
+++ Assets/Scripts/DataProcessor/DataProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataProcessor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FreischaltscheinClass Schein = new FreischaltscheinClass();
+        Schein.Identifier = "MyIdentifier";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
