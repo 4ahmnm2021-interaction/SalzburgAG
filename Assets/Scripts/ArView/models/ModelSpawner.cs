884d044823d482b5aef91fd9a4a6492cb01bdadd
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2021-05-11 15:05:19 +0200

    before api upgrade

--- Assets/Scripts/ArView/models/ModelSpawner.cs
+++ Assets/Scripts/ArView/models/ModelSpawner.cs
@@ -1,25 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ModelSpawner : MonoBehaviour
 {
     public Freischaltschein Schein;
-
+    public DataProcessor DataProcessor;
     public List<GameObject> Prefaps;
     void Start()
     {
-        Schein = gameObject.transform.Find("DataProcessor").GetComponent<DataProcessor>().Schein;
+        Schein = DataProcessor.Schein;
         foreach(var Anlage in Schein.Anlagen) {
             if(Anlage.Type == "Sicherung") {
-                InstantiatePrefap(Prefaps[2], Anlage);
+                var pref = InstantiatePrefap(Prefaps[2], Anlage);
+                SetData(Anlage, Anlage.Type, pref);
             }
         }
         
     }
 
-    void InstantiatePrefap(GameObject prefab, Anlage Anlage) {
-        Instantiate(prefab, Anlage.position, Quaternion.identity);
+    private GameObject InstantiatePrefap(GameObject prefab, Anlage Anlage) {
+        var pref = Instantiate(prefab, Anlage.position,  Quaternion.Euler(0f, 90f, 0f));
+        pref.transform.parent = this.gameObject.transform;
+        pref.transform.position = Anlage.position;
+        pref.transform.localScale = new Vector3(13f,13f,13f);
+        return pref;
+    }
+
+    private void SetData(Anlage Anlage, string type, GameObject obj) {
+        if(type == "Sicherung") {
+            obj.transform.FindChild("Text").GetComponent<Text>().text = Anlage.KKS;
+        }
     }
 
 }
